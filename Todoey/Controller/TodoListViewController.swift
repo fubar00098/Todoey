642e011266225445b96d1946379b4435945c2913
薛宇振 <fubar00098@gmail.com>
2018-01-04 07:44:03 +0800

    Convert to MVC design pattern

--- Todoey/Controller/TodoListViewController.swift
+++ Todoey/Controller/TodoListViewController.swift
@@ -10,31 +10,74 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    var itemArray = ["Find Mike", "Buy Eggos", "Destory Demogorgon"]
+    var itemArray = [Item]()
+    
+    
     
     let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let items = defaults.array(forKey: "TodoListArray") as?[String] {
+        //因為重複使用cell 造成checkMark重複出現,所以使用dictionary增加一個checkMark 檢查用，並使用mvc
+        let newItem = Item()
+        newItem.title = "Find Mike"
+        itemArray.append(newItem)
+        
+        let newItem2 = Item()
+        newItem2.title = "Buy Eggos"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.title = "Destroy Demogorgon"
+        itemArray.append(newItem3)
+        
+
+        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
+        itemArray = items
             
-            itemArray = items
         }
         
     }
+        
+    
     
     //Mark- Tableview Datasource methods
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
         return itemArray.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
         
+        //使用這個方法會創造出新的cell,mark過的cell離開銀幕後會再重新產生，不像dequeueReusableCell重複利用。
+        //let cell = UITableViewCell(style: .default, reuseIdentifier: "ToDoItemCell")
+        
+        
+        //因為重複使用cell 造成checkMark重複出現,所以把array改成使用dictionary增加一個checkMark 檢查用，並使用mvc 創造一個item Model
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        let item = itemArray[indexPath.row]
+        
+        
+        cell.textLabel?.text = item.title
+        
+        
+        //Ternary operatoe ==>
+        //value = condition ? valueIfTrue : valueIfFalse
+        cell.accessoryType = item.done ? .checkmark : .none
+        
+
+//        if item.done == true {
+//
+//            cell.accessoryType = .checkmark
+//        }else {
+//
+//            cell.accessoryType = .none
+//        }
+
         return cell
     }
     
@@ -44,26 +87,33 @@ class TodoListViewController: UITableViewController {
     //感應TableView觸碰的方法
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //print(itemArray[indexPath.row])
-        
         //選取後的反黑會消失
         tableView.deselectRow(at: indexPath, animated: true)
         
-        //Use If/else to make a checkmark when touch and display
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-            
-        }else {
-            
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
         
+        //use opposite
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
+//
+//        if itemArray[indexPath.row].done == false {
+//
+//            itemArray[indexPath.row].done = true
+//        }else{
+//
+//            itemArray[indexPath.row].done = false
+//        }
+
+        //forces the tableView to call data source method again
+        
+        tableView.reloadData()
     }
+    
+    
+    
+    
     
     //MARK - Add New Items
     
-
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
         //use Global variable that alertTextfield can available show in UIAlertAction
@@ -73,9 +123,12 @@ class TodoListViewController: UITableViewController {
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //What will happen once the user clicks the Add Item button on our UIAlert
-            print(TextField.text)
+            print(TextField.text!)
             
-            self.itemArray.append(TextField.text!)
+            let newItem = Item()
+            newItem.title = TextField.text!
+            
+            self.itemArray.append(newItem)
             
             self.defaults.set(self.itemArray, forKey: "TodoListArray")
             
---
+++ Todoey/Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Spoke on 2018/1/3.
+//  Copyright © 2018年 Spoke. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+}
